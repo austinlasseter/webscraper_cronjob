ab48e006181b1c0a735c269f55e453814063635b
Austin Lasseter <austinlasseter@gmail.com>
2020-11-18 21:42:12 -0500

    update app.py

--- app.py
+++ app.py
@@ -1,25 +1,3 @@
-# # from flask import Flask, render_template
-# # app = Flask(__name__)
-# #
-# # @app.route('/')
-# # def home():
-# #    return render_template('index.html')
-# #
-# # if __name__ == '__main__':
-# #    app.run()
-# from flask import Flask, request
-# # set the project root directory as the static folder, you can set others.
-# app = Flask(__name__, static_url_path='')
-#
-# @app.route('/')
-# def root():
-#     return app.send_static_file('index.html')
-#
-# if __name__ == "__main__":
-#     app.run()
-
-# serve.py
-
 from flask import Flask
 from flask import render_template
 
@@ -34,4 +12,4 @@ def hello():
 
 # run the application
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0')

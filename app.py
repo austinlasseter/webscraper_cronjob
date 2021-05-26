9c8467e948393c0b998b4b515888cc26f77fae14
Austin Lasseter <austinlasseter@gmail.com>
2021-05-26 19:09:28 -0400

    updates

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from flask import render_template
 # creates a Flask application, named app
 app = Flask(__name__)
 
-# a route where we will display a welcome message via an HTML template
+# display HTML template
 @app.route("/")
-def hello():
-    message = "Hello, World"
-    return render_template('index.html', message=message)
+def display_index():
+    return render_template('index.html')
 
-# run the application
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+
+############ Deploy
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
